4586cbea3cac1a7f8cc795b7bb2f7e02b602bfb0
Clement <clementduchene0803@gmail.com>
2022-11-23 09:58:50 +0100

    <feat>(controller)

---
+++ Micromaniak/src/Controller/PlateformeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PlateformeController extends AbstractController
+{
+    #[Route('/plateforme', name: 'app_plateforme')]
+    public function index(): Response
+    {
+        return $this->render('plateforme/index.html.twig', [
+            'controller_name' => 'PlateformeController',
+        ]);
+    }
+}
