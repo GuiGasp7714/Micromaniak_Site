efc97bb6e5373a1c2f1f034310ad0182e37a68ad
GuiGasp1477 <luffy272727@gmail.com>
2022-12-07 12:29:40 +0100

    Consultation fonctionnelle, non mise en forme

---
+++ Micromaniak/src/Controller/MicromaniakController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MicromaniakController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_micromaniak")
+     */
+    public function index(): Response
+    {
+        return $this->render('micromaniak/index.html.twig', [
+            'controller_name' => 'MicromaniakController',
+        ]);
+    }
+    
+}
--- Micromaniak/src/Controller/PlateformeController.php
+++ Micromaniak/src/Controller/PlateformeController.php
@@ -5,14 +5,26 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Plateforme;
+use App\Entity\Marque;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlateformeController extends AbstractController
 {
-    #[Route('/plateforme', name: 'app_plateforme')]
-    public function index(): Response
+    /**
+     * @Route("/plateforme", name="plateforme")
+     */
+    public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $repos = $this->getDoctrine()->getRepository(Plateforme::class);
+        $LesPlateformes = $repos->findAll();       
+
         return $this->render('plateforme/index.html.twig', [
             'controller_name' => 'PlateformeController',
+            'LesPlateformes'=> $LesPlateformes,
         ]);
     }
 }
