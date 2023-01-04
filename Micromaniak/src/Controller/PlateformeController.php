6539cffa3fcecf675dc729fe3c8d833d947eec2f
GuiGasp1477 <luffy272727@gmail.com>
2023-01-04 09:53:42 +0100

    plateforme apparemment

--- Micromaniak/src/Controller/PlateformeController.php
+++ Micromaniak/src/Controller/PlateformeController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Plateforme;
+use App\Form\PlateformeType;
 use App\Entity\Marque;
-use Doctrine\Persistence\ObjectManager;
-use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Component\Form\Extension\HttpFoundation\HttpFoundationExtension;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Forms;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+
 
 class PlateformeController extends AbstractController
 {
@@ -19,11 +24,24 @@ class PlateformeController extends AbstractController
      */
     public function index(Request $request, EntityManagerInterface $entityManager): Response
     {
+        $plateforme = new Plateforme();
+        
+        $formPlateforme = $this->createForm(PlateformeType::class, $plateforme);
+
+        $formPlateforme->handleRequest($request);
+        if ($formPlateforme->isSubmitted() && $formPlateforme->isValid()) {
+            die.dump($plateforme);
+            $entityManager->$persist($plate56+orme);
+            $entityManager->flush();
+            return $this->redirectToRoute('plateforme');
+        }
+
         $repos = $this->getDoctrine()->getRepository(Plateforme::class);
         $LesPlateformes = $repos->findAll();       
 
         return $this->render('plateforme/index.html.twig', [
             'controller_name' => 'PlateformeController',
+            'formPlateforme' => $formPlateforme->createView(),
             'LesPlateformes'=> $LesPlateformes,
         ]);
     }
